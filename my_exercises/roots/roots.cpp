b7dd5ef3fd381498e6a89b758b5966c1745eceba
tbellotti <thomas.bellotti@polytechnique.edu>
2018-10-08 09:53:50 +0200

    Added the base concept for finding the roots of a polynomial function

---
+++ my_exercises/roots/roots.cpp
@@ -0,0 +1,44 @@
+// Project for the search of the roots of 
+// a given polynomial function.
+
+/*
+   |Function|
+==============
+- coefficients
+==============
++ Function()
++ eval (x)
++ print()
++ derivative
+*/
+
+
+/*
+ !Bisection!
+==============
+- function
+- inf_lim
+- sup_lim
+- tolerance
+- termination criterion
+- max_iterations
+==============
++ Bisection()
++ find_root(n_iter)
+- conver_criterion()
+*/
+
+/*
+ !Newton!
+==============
+- function
+- function_derivative
+- x_init
+- tolerance
+- termination criterion
+- max_iterations
+==============
++ Newton()
++ find_root(n_iter)
+- conver_criterion()
+*/
