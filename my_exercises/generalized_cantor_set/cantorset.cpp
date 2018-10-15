ffc1b23f44460314e567c444e45bdeca0683e3ec
tbellotti <thomas.bellotti@polytechnique.edu>
2018-10-15 08:27:51 +0200

    Minor changes

--- my_exercises/generalized_cantor_set/cantorset.cpp
+++ my_exercises/generalized_cantor_set/cantorset.cpp
@@ -43,7 +43,9 @@ void createCantorSetTikz (std::string filename, unsigned int n_iter, double my_e
 
 	// Just some content to use tikZ
 	draw_file<<"\\documentclass[10pt, a4paper]{article}"<<endl;
-	draw_file<<"\\usepackage{amsmath}"<<"\\usepackage{tikz}"<<endl<<"\\begin{document}"<<"\\textbf{Generalized Cantor Set} with $\\epsilon = $"<<my_eps<<endl<<endl<<"\\begin{tikzpicture}"<<endl;
+	draw_file<<"\\usepackage{amsmath}"<<"\\usepackage{tikz}"<<endl;
+	draw_file<<"\\begin{document}"<<endl;
+	draw_file<<"\\textbf{Generalized Cantor Set} with $\\epsilon = $"<<my_eps<<endl<<endl<<"\\begin{tikzpicture}"<<endl;
 
  	// Creating the base segment for the set. 
 	double y = 0.0; // y-cordinate to increment according to spacing at each iteration.
@@ -52,7 +54,7 @@ void createCantorSetTikz (std::string filename, unsigned int n_iter, double my_e
 	for (unsigned int c_it = 0; c_it < n_iter; ++c_it)	{
 		
 		for (auto it = my_set.subsets.cbegin(); it != my_set.subsets.cend(); ++it)	{
-			draw_file<<"\\draw [line width=2.0mm] ("<<factor*(it->getBegin())<<","<<y<<") -- ("<<factor*(it->getEnd())<<","<<y<<");"<<endl;	
+			draw_file<<"\\draw [blue, line width=2.0mm] ("<<factor*(it->getBegin())<<","<<y<<") -- ("<<factor*(it->getEnd())<<","<<y<<");"<<endl;	
 		}
 		my_set.oneDivision();
 		y += spacing;		
@@ -63,7 +65,6 @@ void createCantorSetTikz (std::string filename, unsigned int n_iter, double my_e
 	// To finish the file.
 	draw_file<<"\\end{tikzpicture}"<<endl<<"\\end{document}"<<endl;	
 
-
 	// CLosing the file stream
 	draw_file.close();
 }	
@@ -71,18 +72,8 @@ void createCantorSetTikz (std::string filename, unsigned int n_iter, double my_e
 // Testing routine
 int main ()
 {
-	CantorSet my_c_set (1.0, 0.0, 1.0);
-	//my_c_set.print();	
-
-	//my_c_set.oneDivision();
-
-	//my_c_set.print();
-
-	my_c_set.someDivisions(10);
-	my_c_set.print();
-
 	string fname("cantorset");
-	createCantorSetTikz (fname, 14, 0.6, 0.3);
+	createCantorSetTikz (fname, 10, 0.4, 0.3);
 
 
 	string call = "pdflatex "+fname+".tex";
